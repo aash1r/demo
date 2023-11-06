92ad73cca9cbbab58db2e6257c61d512d1d3fc3e
aashir <aashirisani@gmail.com>
2023-11-07 01:14:12 +0500

    demo 2

--- lib/demo_screen.dart
+++ lib/demo_screen.dart
@@ -6,25 +6,44 @@ class DemoScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(backgroundColor: const Color.fromARGB(184, 0, 43, 17),body: SingleChildScrollView(
-      child: Column(children: [const SizedBox(height: 10,),const ListTile(leading: Icon(Icons.cloud,color: Colors.white,size: 40,),trailing: Icon(Icons.person,color: Colors.white,size: 40),),
+    return  Scaffold(backgroundColor: const Color.fromARGB(184, 0, 43, 17),
+      body: SingleChildScrollView(
+      child: Column(children: [
+        const SizedBox(height: 10,),
+        const ListTile(leading: Icon(Icons.cloud,color: Colors.white,size: 40,),
+        trailing: Icon(Icons.person,color: Colors.white,size: 40),),
       const SizedBox(height: 20,),
       Container(
-                  height: 610,
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30),
+        height: 610,
+        width: double.infinity,
+         decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
                       ),),
-                      child:  Padding(
+                child:Padding(
                         padding: const EdgeInsets.all(18.0),
-                        child: Column(crossAxisAlignment: CrossAxisAlignment.start,children: [const SizedBox(height: 40,),const Text("Hello There",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),const SizedBox(height: 18,),const Text("let's begin scanning with one of the following1",style: TextStyle(fontWeight: FontWeight.w400,fontSize: 17),),
-                        const SizedBox(height: 100,),CustomContainer(ontap: (){},text: "Manual Scanning",),const SizedBox(height: 35,),CustomContainer(ontap: () {},text:"Bot Scanning")],),
-                      ),
-                      ),
-                      ],),
+                        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 40,),
+                          
+                          const Text("Hello There",
+                            style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
+                          
+                          const SizedBox(height: 18,),
+                        
+                          const Text("let's begin scanning with one of the following!",
+                            style: TextStyle(fontWeight: FontWeight.w400,fontSize: 17),),
+                        
+                          const SizedBox(height: 100,),
+                          
+                          CustomContainer(ontap: (){},text: "Manual Scanning",),
+                          
+                          const SizedBox(height: 35,),
+                          
+                          CustomContainer(ontap: () {},text:"Bot Scanning")],),
+                      ),),],),
     ),);
   }
 }
--- lib/widgets/custom_container.dart
+++ lib/widgets/custom_container.dart
@@ -7,6 +7,17 @@ class CustomContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(onTap: ontap, child: Container(width: double.infinity,height:70,decoration:   BoxDecoration(color: const Color.fromARGB(244, 0, 43, 17),borderRadius:BorderRadius.circular(10)) ,child: Center(child: Text(text,style: const TextStyle(color: Colors.white),)),));
+    return GestureDetector(
+      onTap: ontap, 
+      child: Container(
+        width: double.infinity,
+        height:70,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(244, 0, 43, 17),
+          borderRadius:BorderRadius.circular(10)) ,
+          child: Center(child: Text(text,
+              style: const TextStyle(color: Colors.white),),
+                ),),
+                  );
   }
 }
